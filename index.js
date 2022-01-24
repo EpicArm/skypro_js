f695165e8394471b6d6df7bf01c58a35d9feddce
EpicArm <EpicArm@users.noreply.github.com>
2022-01-24 20:27:41 +0000

    Deploying to gh-pages from @ EpicArm/skypro_js@9ca9770aa6b36bb06efec64eab56d4eb09eaed3b 🚀

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 let nameProduct = "Solingen";
 let priceProduct = 100;
-let infoProduct = nameProduct + " " + "цена" + " " + priceProduct + " " + "рублей";
-alert(infoProduct)
+let infoProduct = `${nameProduct}, цена ${priceProduct} рублей`;
+alert(infoProduct);
